24257b20cbd7026ed248770f2c13e897a36bd5e4
newaz2796 <newaz2796@gmail.com>
2025-07-19 00:06:36 +0600

    footer design added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -10,16 +10,16 @@ const Footer = () => {
       });
     }, []);
   return (
-    <div className="bg-blue-700 text-white dark:bg-gray-900 ">
+    <div className="bg-gray-200 text-white dark:bg-gray-900 ">
       <footer className="footer sm:footer-horizontal py-10 px-5 lg:px-20">
         <aside>
          <img className="h-16 w-16 rounded-full" data-aos="fade-up"  src={`https://i.ibb.co/tPJs4Gv5/home-06.jpg`} alt="" />
-          <h2 className="text-2xl" data-aos="fade-Left">Home Repair Services</h2>
-          <p data-aos="fade-left">
+          <h2 className="text-2xl text-blue-500 font-extrabold" data-aos="fade-Left">Home Repair Services</h2>
+          <p data-aos="fade-left" className="text-black dark:text-white">
             Providing reliable service since 2010
           </p>
         </aside>
-        <nav>
+        <nav className="text-black dark:text-white">
           <h6 className="footer-title">Services</h6>
           <Link to='/allServices'>All Service</Link>
           <Link to='/manageServices'>Manage Service</Link>
@@ -28,21 +28,21 @@ const Footer = () => {
 
           
         </nav>
-        <nav>
+        <nav className="text-black dark:text-white">
           <h6 className="footer-title">Follow Us</h6>
           <a href='https://web.facebook.com/shahnewaz.ahammed' target="_blank" className="link link-hover">Facebook</a>
           <a href="https://www.linkedin.com/jobs/" target="_blank" className="link link-hover">LinkedIn</a>
           <a href="https://www.instagram.com/blue_bird_himu/" className="link link-hover">Instagram</a>
           <a href='https://github.com/newaz2580' target="_blank" className="link link-hover">Github</a>
         </nav>
-        <nav>
+        <nav className="text-black dark:text-white">
           <h6 className="footer-title">Legal</h6>
           <a className="link link-hover">Terms of use</a>
           <a className="link link-hover">Privacy policy</a>
           <a className="link link-hover">Cookie policy</a>
         </nav>
       </footer>
-      <h3 className="text-center py-5">
+      <h3 className="text-center py-5 text-black dark:text-white">
         © Copyright Home Repair Services All Rights Reserved
       </h3>
     </div>
